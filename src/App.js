23ee22afc61bbc16963e0772a1505bbf823fc4a1
YOUR NAME <ritter.christina.r.c@gmail.com>
2023-02-14 16:06:40 +0100

    added delete feat

--- src/App.js
+++ src/App.js
@@ -1,59 +1,64 @@
 import "./App.css";
 import Form from "./components/Form";
 import List from "./components/List";
-import { useLocalStorage } from '@rehooks/local-storage';
+import { useLocalStorage } from "@rehooks/local-storage";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [activities, setActivities] = useLocalStorage("activityList", [] );
+  const [activities, setActivities] = useLocalStorage("activityList", []);
   // const [activityList, setNewActivities] = useLocalStorage("activityList", {defaultValue: []})
-  console.log(activities)
-  const handleAddActivity= (activityObject) =>{
-    setActivities([...activities,activityObject])
-  }
-  
-  const [weather, setWeather] = useState('');
-  
+  // console.log(activities);
+  const handleAddActivity = (activityObject) => {
+    setActivities([...activities, activityObject]);
+  };
+
+  const handleDeleteActivity = (id) => {
+    setActivities(activities.filter(activity => activity.id !==id))
+  };
+  // console.log(activities.id)
+
+  const [weather, setWeather] = useState("");
+
   setInterval(() => {
-    clearInterval()
+    clearInterval();
   }, 5000);
 
-
   useEffect(() => {
-    loadWeather()
-    const timer = setInterval(loadWeather, 5000)
+    loadWeather();
+    const timer = setInterval(loadWeather, 5000);
     return () => {
-      clearInterval(timer)
+      clearInterval(timer);
+    };
+  }, []);
+
+  async function loadWeather() {
+    try {
+      const response = await fetch(
+        "https://example-apis.vercel.app/api/weather/europe"
+      );
+      const data = await response.json();
+
+      setWeather(data);
+    } catch (error) {
+      console.log(error);
     }
-    },[])
-
-    async function loadWeather() {
-      try {
-        const response = await fetch("https://example-apis.vercel.app/api/weather/europe");
-        const data = await response.json();
-
-        setWeather(data);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-
+  }
 
   return (
     <div className="">
       <header className="weather">
-      <div>{weather.location}</div>
-      <div>{weather.temperature}°</div>
-      <div>{weather.condition}</div>
+        <div>{weather.location}</div>
+        <div>{weather.temperature}°</div>
+        <div>{weather.condition}</div>
       </header>
-      <List activity={activities} weather={weather}></List>
+      <List
+        activity={activities}
+        weather={weather}
+        onDeleteActivity={handleDeleteActivity}
+      ></List>
       <Form onAddActivity={handleAddActivity}></Form>
     </div>
   );
 }
 
 export default App;
-
-
-
-
--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,18 +1,35 @@
-import "./List.css"
+import "./List.css";
 
-export default function List({activity, weather}) {
-    const isGoodWeather = weather.isGoodWeather
-    console.log(weather)
-    console.log(isGoodWeather)
+export default function List({ activity, weather, onDeleteActivity }) {
+  const isGoodWeather = weather.isGoodWeather;
+  console.log(weather);
+  console.log(isGoodWeather);
 
-    return ( 
-        <>
-        <div className="list">
-            <h2> {isGoodWeather ? 'Go outside and enjoy the good weather!' : 'Bad weather, stay in your basement!'}</h2>
-            {activity.filter(act => act.isForGoodWeather===isGoodWeather).map(filteredActivities => (
-                <div className="list--item" key={filteredActivities.id}>{filteredActivities.name}</div>
-            ))}
+  return (
+    <>
+      <div className="list">
+        <h2>
+          {" "}
+          {isGoodWeather
+            ? "Go outside and enjoy the good weather!"
+            : "Bad weather, stay in your basement!"}
+        </h2>
+        {activity
+          .filter((activity) => activity.isForGoodWeather === isGoodWeather)
+          .map((filteredActivities) => (
+            <div>
+              {" "}
+              <div className="list--item" key={filteredActivities.id}>
+                {filteredActivities.name}
+              </div>
+              <button
+                className=""
+                type="button"
+                onClick={() => onDeleteActivity?.(filteredActivities.id)}
+              >x</button>
             </div>
-        </>
-    )   
+          ))}
+      </div>
+    </>
+  );
 }
