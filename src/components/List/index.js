49a032d9af96f3d369b69bcf8a55833ff67a0a44
YOUR NAME <ritter.christina.r.c@gmail.com>
2023-02-14 15:02:04 +0100

    weather-filter implemented

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,14 +1,31 @@
 import "./List.css"
 
+// export default function List({activity}) {
+//     console.log(activity)
+//     console.log(activity[0].name)
+
+//     return ( 
+//         <div className="list">
+//         {activity.map((a) => { 
+//         return <div key={activity.id}>{a.name}</div> })}
+//         </div>
+//     )   
+// }
+
 export default function List({activity}) {
     console.log(activity)
-    console.log(activity[0].name)
+    // console.log(activity[0].name)
+
+    const isGoodWeather = true;
 
     return ( 
+        <>
+        <h2>Go outside and enjoy the good weather!</h2>
         <div className="list">
-        {activity.map((a) => { 
-        return <div key={activity.id}>{a.name}</div> })}
-        </div>
+            {activity.filter(act => act.isForGoodWeather===isGoodWeather).map(filteredActivities => (
+                <div key={filteredActivities.id}>{filteredActivities.name}</div>
+            ))}
+            </div>
+        </>
     )   
 }
-
