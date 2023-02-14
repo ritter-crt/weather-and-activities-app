27a5a12409e25567a738cd05a852d4ba70458133
YOUR NAME <ritter.christina.r.c@gmail.com>
2023-02-14 11:50:14 +0100

    submit-form created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Form from "./components/Form";
+import { useState } from "react";
 
 function App() {
+  const [activity, setActivity] = useState({});
+  
+  const handleAddActivity= (activity) =>{
+    setActivity(activity)
+  }
+  
+console.log(activity)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <header className="App-header"> Weather App</header>
+      <Form onAddActivity={handleAddActivity}></Form>
     </div>
   );
 }
